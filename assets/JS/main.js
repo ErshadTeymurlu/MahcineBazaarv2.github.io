874c3681411f21e9e8ff940da3daa76761f2251c
ErshadTeymurlu <7ahgfnr@code.edu.az>
2022-08-06 11:08:29 +0400

    changed

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -63,3 +63,22 @@ $(document).ready(function () {
         }
     });
 });
+$(document).ready(function () {
+    $(".owlCarousel").owlCarousel({
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                loop: false
+            }
+        }
+    });
+});
